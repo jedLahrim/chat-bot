87edcffe65ecfb7d750c79fd59d8e87a6c6dc32c
jed <jolix123@gmail.com>
2023-10-01 15:59:15 +0100

    commit 66

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -43,6 +43,8 @@ function App() {
   return (
     <main>
       <h1>CHAT BOT AI</h1>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2795257946759876"
+                crossOrigin="anonymous"></script>
       <section>
         {chats && chats.length
           ? chats.map((chat, index) => (
